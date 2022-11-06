99754af82a1712ceef8928587c5ee3c3b5d7dfb4
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2022-11-06 19:16:16 +0300

    My first tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ruslan_slepuhin_evolution
-HomeTask Lesson 4
+Hello!
+For runing thees tests you need to do the next steps:
+1. clone this repository to your local workstation
+2. move into the project directory
+3. type and execute 'yarn install' in the console for installing all modules describe in package.json
+4. type and execute 'yarn test' for runing the tests in ./tests/rectangle.test.js for the script in ./js/rectangle.js
+5. enjoy)
---
+++ tests/rectangle.test.js
@@ -0,0 +1,41 @@
+const rectang = require('../js/rectangle')
+
+/// function getRectangleArea testing
+// the first test
+test('should properly output the area value', () => {
+    const outputText = rectang.getRectangleArea(4, 5);
+    expect(outputText).toBe(20);
+});
+
+// the second test
+test('should properly output the area value', () => {
+    const outputText = rectang.getRectangleArea(435, 1024);
+    expect(outputText).toBe(445440);
+});
+
+
+// function getRectanglePerimeter testing
+// the first test
+test('should properly output the perimeter value', () => {
+    const outputText = rectang.getRectanglePerimeter(4, 5);
+    expect(outputText).toBe(18);
+});
+
+// the second test
+test('should properly output the perimeter value', () => {
+    const outputText = rectang.getRectanglePerimeter(1, 4556);
+    expect(outputText).toBe(9114);
+});
+
+// function getRectangleInfo testing
+// the first test
+test('should properly output the message to consol', () => {
+    const outputText = rectang.getRectangleInfo(6, 12);
+    expect(outputText).toBe(console.log('The perimeter of a rectangle is 36 and the area is 72'))
+});
+
+// the second test
+test('should properly output the message to consol', () => {
+    const outputText = rectang.getRectangleInfo(24, 600);
+    expect(outputText).toBe(console.log('The perimeter of a rectangle is 1248 and the area is 14400'))
+});
